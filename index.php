97278f84a7faf80dc5676a7a7927f67c74948ba8
Dylan Swartz <mail@dylanswartz.com>
2011-07-13 15:24:16 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title>SVSU Database Frontend</title>
+
+<link rel="stylesheet" href="css/style.css" type="text/css">
+ <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script> 
+<script type="text/javascript" src="js/scripts.js"></script>
+</head>
+<body>
+	<div id="wrapper">
+		<div id="header">SVSU Database App</div>
+
+		<div id="content">
+
+			<div id="form">
+			
+			</div>
+
+		</div>
+	
+		<div id="footer"></div>
+	</div>
+	</body>
+	</html>
