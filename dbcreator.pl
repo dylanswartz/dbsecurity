0e417975d1c440b89c1933133330924cf685d920
Dylan Swartz <mail@dylanswartz.com>
2011-07-18 16:08:20 -0400

    blah

--- dbcreator.pl
+++ dbcreator.pl
@@ -10,7 +10,7 @@ my($host)  = "localhost";
 my($database)  = "test_db";
 my($tablename) = "jobs";
 my($username)  = "developer";
-my($password)  = "devpassword";
+my($password)  = "SVSUd3v3lop3r";
 
 my($dbh) = DBI->connect("DBI:mysql:${database};host=${host}", $username, $password, 
 			   { RaiseError => 1 }
